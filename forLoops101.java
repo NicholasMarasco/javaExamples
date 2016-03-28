42f491ea4944fd785825af90a8f1eb128e99a8a5
Nicholas Marasco <marascnd197@potsdam.edu>
2016-03-28 13:47:13 -0400

    This is the start of something terrible.

---
+++ forLoops101.java
@@ -0,0 +1,12 @@
+public class forLoops101{
+  public static void main(String[] args){
+    int i = 0;
+    for(;true;)
+    { 
+      i++;
+      if( i == 5 )
+      {break;}
+      continue;
+    }
+  }
+}
