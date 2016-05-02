635d15c6ac98fb8cd578c6b97681d12932284d7f
Nicholas Marasco <marascnd197@potsdam.edu>
2016-05-02 11:30:22 -0400

    Gotta check them errors Also fixed README title

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# javaExamples
+# Java Examples
 The difference between "Can I?" and "Should I?"
 
 DISCLAIMER:
-  
-    -Do not take this seriously. 
-  
+
+    -Do not take this seriously.
+
     -This is a monsterous creation of all the worst things you could possibly do.
-  
+
     -This contains the worst coding practices and biggest waste of resources that still accomplishes something.
-  
+
     -If you plan to learn anything from this, let it be to NEVER do any of this.
-  
+
     -If this offends you, I understand. It hurts me to write it as much as it hurts you to read it.
--- forLoops101.java
+++ forLoops101.java
@@ -1,14 +1,20 @@
 public class forLoops101{
   public static void main(String[] args){
-    I i = new I();
-    i.initializeToBeZero();
-    for(;true;)
-    { System.out.println(i);
-      i = i.pp();
-      if(Five.checkifequaltofive(i))
-      {break;}
-      continue;
+    do
+    { try
+      { I i = new I();
+        i.initializeToBeZero();
+        for(;true;)
+        { System.out.println(i);
+          i = i.pp();
+          if(Five.checkifequaltofive(i))
+          {break;}
+          else
+          {continue;}
+        }
+        return;
+      } catch(Exception e){}
     }
-    return;
+    while(false);
   }
 }
